c78a15b376eec19a6643078e12d24ba7ae44c5f2
Jack <jcruse_mulhall@stuy.edu>
2017-11-02 15:36:48 -0400

    made rwx

--- stat.c
+++ stat.c
@@ -5,6 +5,45 @@
 #include <sys/stat.h>
 #include <time.h>
 
+void print_an_oct(int das) {
+  if (!das) {
+    printf("---");
+  }
+  else if (das == 1) {
+    printf("--x");
+  }
+  else if (das == 2) {
+    printf("-w-");
+  }
+  else if (das == 3) {
+    printf("-wx");
+  }
+  else if (das == 4) {
+    printf("r--");
+  }
+  else if (das == 5) {
+    printf("r-x");
+  }
+  else if (das == 6) {
+    printf("rw-");
+  }
+  else if (das == 7) {
+    printf("rwx");
+  }
+}
+
+	   
+void print_oct(int sad) {
+  if (!sad) {
+    return;
+  }
+  else {
+    print_oct(sad/8);
+    print_an_oct(sad%8);
+  }
+}
+  
+
 int main(){
   struct stat buf;
   if(stat("stat.c", &buf) == -1) {
@@ -24,7 +63,11 @@ int main(){
     
     printf("File size: %dGB %dMB %dKB %dB\n",gb, mb, kb, b);
     unsigned int perms_filter = 511;
-    printf("File permissions: %o\n",(buf.st_mode & perms_filter));//bitwise and to get rid of the first 3 numbers
-    printf("Time last accessed: %s\n",ctime(&buf.st_atime)); 
+    unsigned int perm = buf.st_mode & perms_filter;
+    printf("File permissions: %o\n",(perm));//bitwise and to get rid of the first 3 numbers
+    printf("Time last accessed: %s\n",ctime(&buf.st_atime));
+
+    print_oct(perm);
+    printf("\n");
   }
 }
